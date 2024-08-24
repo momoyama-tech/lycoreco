13dee54dd444475dc0a19e577a01fb04575988d9
yamagumo <131755296+yamagumo@users.noreply.github.com>
2024-08-25 04:51:11 +0900

    タイトル作成

--- front/lycoreco_front/src/routes/+page.svelte
+++ front/lycoreco_front/src/routes/+page.svelte
@@ -1,5 +1,43 @@
 <main>
-	こころ
+	<p class="text">Title</p>
+	<h3 class="tamago">Playing games and voice chat</h3>
+	<center>
+        <ion-button shape="round">
+            Play now!
+        </ion-button>
+    </center>
+	<img src="supanyan.png">
 </main>
 
-<style></style>
+<style>
+	 .text {
+        margin: 0 auto;
+        display: block;
+	    position: absolute; 
+        top: 50%;
+    	text-align: center;
+    	width: 100%;
+        font-size: 50px;
+        line-height: 12px;
+		font-weight: bold;
+             }
+	.tamago {
+		margin: 0 auto;
+        display: block;
+	    position: absolute; 
+        top: 55%;
+    	text-align: center;
+    	width: 100%;
+        line-height: 12px;
+		font-weight: bold;
+	}
+	ion-button {
+            margin: 0 auto;
+            display: ruby-text-container;
+            position: fixed; 
+            bottom: 20px; 
+            width: 30%; 
+
+             }
+
+</style>
