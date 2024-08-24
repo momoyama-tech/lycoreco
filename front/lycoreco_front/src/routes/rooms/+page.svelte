31407ea1ecf0c4bcd77209df67e60a7f933cc507
yuki-kunn <21e1248@andrew.ac.jp>
2024-08-24 22:00:05 +0900

    conflict resolve

---
+++ front/lycoreco_front/src/routes/rooms/+page.svelte
@@ -0,0 +1,42 @@
+<main>
+	<h3>戻る</h3>
+    <center>
+        <h1>ルームリスト</h1>
+    </center>
+		<ion-card>
+			<ion-card-header>
+				<ion-card-title>ルーム1</ion-card-title>
+			</ion-card-header>
+		</ion-card>
+		<ion-card>
+			<ion-card-header>
+				<ion-card-title>ルーム2</ion-card-title>
+			</ion-card-header>
+		</ion-card>
+		<ion-card>
+			<ion-card-header>
+				<ion-card-title>ルーム3</ion-card-title>
+			</ion-card-header>
+		</ion-card>
+		<ion-card>
+			<ion-card-header>
+				<ion-card-title>ルーム4</ion-card-title>
+			</ion-card-header>
+		</ion-card>
+		<ion-card>
+			<ion-card-header>
+				<ion-card-title>ルーム5</ion-card-title>
+			</ion-card-header>
+		</ion-card>
+	<center>
+		<ion-button shape="round">
+			＋
+		</ion-button>
+	</center>
+</main>
+<style>
+	ion-botton{
+		text-align: center;
+		position: absolute;
+	}
+</style>
---
+++ front/lycoreco_front/src/routes/waiting/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	function onclick() {
+		console.log('clicked');
+	}
+</script>
+
+<main>
+	<ion-header>
+		<ion-button {onclick} size="small" class="back_button" fill="clear">←</ion-button>
+		<h1 class="text_center">メンバーリスト</h1>
+	</ion-header>
+	<ion-card>
+		<ion-card-header>
+			<ion-card-title>メンバー１</ion-card-title>
+		</ion-card-header>
+	</ion-card>
+	<ion-card>
+		<ion-card-header>
+			<ion-card-title>メンバー２</ion-card-title>
+		</ion-card-header>
+	</ion-card>
+	<ion-card>
+		<ion-card-header>
+			<ion-card-title>メンバー３</ion-card-title>
+		</ion-card-header>
+	</ion-card>
+	<ion-card>
+		<ion-card-header>
+			<ion-card-title>メンバー４</ion-card-title>
+		</ion-card-header>
+	</ion-card>
+</main>
+
+<style>
+	.header {
+		background-color: #ffffff;
+	}
+
+	.text_center {
+		text-align: center;
+	}
+	.bottom_space {
+		line-height: 30px;
+	}
+
+	.back_button {
+		font-size: 25px;
+	}
+</style>
