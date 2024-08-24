67078e79aa19bd0232e71bf0a2b498c2cf8f5ccb
yamagumo <131755296+yamagumo@users.noreply.github.com>
2024-08-24 22:53:32 +0900

    roomsの作成

--- front/lycoreco_front/src/routes/rooms/+page.svelte
+++ front/lycoreco_front/src/routes/rooms/+page.svelte
@@ -1,5 +1,44 @@
 <main>
-	くも
+    <ion-header>
+        <ion-button {onclick} size="small" class="back_button" fill="clear">←</ion-button>
+    </ion-header>
+    <center>
+        <h1>ルームリスト</h1>
+    </center>
+        <ion-card>
+            <ion-card-header>
+                <ion-card-title>ルーム1</ion-card-title>
+            </ion-card-header>
+        </ion-card>
+        <ion-card>
+            <ion-card-header>
+                <ion-card-title>ルーム2</ion-card-title>
+            </ion-card-header>
+        </ion-card>
+        <ion-card>
+            <ion-card-header>
+                <ion-card-title>ルーム3</ion-card-title>
+            </ion-card-header>
+        </ion-card>
+        <ion-card>
+            <ion-card-header>
+                <ion-card-title>ルーム4</ion-card-title>
+            </ion-card-header>
+        </ion-card>
+        <ion-card>
+            <ion-card-header>
+                <ion-card-title>ルーム5</ion-card-title>
+            </ion-card-header>
+        </ion-card>
+    <center>
+        <ion-button shape="round">
+            ＋
+        </ion-button>
+    </center>
 </main>
-
-<style></style>
+<style>
+    ion-botton{
+        text-align: center;
+        position: absolute;
+    }
+</style>
