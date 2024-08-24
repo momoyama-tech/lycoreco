6f74483ed90a007f2987d87451dc214641bdf535
yuki-kunn <21e1248@andrew.ac.jp>
2024-08-24 19:33:00 +0900

    waiting画面作成開始

--- front/lycoreco_front/src/routes/waiting/+page.svelte
+++ front/lycoreco_front/src/routes/waiting/+page.svelte
@@ -1,5 +1,35 @@
+<script></script>
+
 <main>
-	yu-ki
+	<ion-header>
+		<h3><ion-button size="small" class="back_button" fill="clear">←</ion-button></h3>
+		<h1 class="text_center">メンバーリスト</h1>
+	</ion-header>
+	<ion-card class="bottom_space">
+		<ion-card-header>
+			<ion-card-title>Card Title</ion-card-title>
+			<ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+		</ion-card-header>
+
+		<ion-card-content>
+			Here's a small text description for the card content. Nothing more, nothing less.
+		</ion-card-content>
+	</ion-card>
 </main>
 
-<style></style>
+<style>
+	.header {
+		background-color: #ffffff;
+	}
+
+	.text_center {
+		text-align: center;
+	}
+	.bottom_space {
+		line-height: 30px;
+	}
+
+	.back_button {
+		font-size: 25px;
+	}
+</style>
